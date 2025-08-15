218d19a009a63bdb7f37bfd5819da0a690a703b8
hwas2620 <hwas2620@gmail.com>
2025-08-16 01:05:30 +0900

    ArticleCard 구조 일부 변경

--- practices/02/src/components/common/ArticleCard.jsx
+++ practices/02/src/components/common/ArticleCard.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import './ArticleCard.css';
 import InitialsIcon from './InitialsIcon';
 
-function ArticleCard({ as: Component = 'div', variant, articleInfo }) {
+function ArticleCard({ as: Component = 'div', variant, pos, articleInfo }) {
     return (
-        <Component className={`article-card ${variant}`}>
+        <Component className={`article-card ${variant} ${pos}`}>
             <a href={`#article_${articleInfo.id}`} className="article-card-wrapper">
                 <div className="article-img-container">
                     <img src={articleInfo.thumbnail} alt={articleInfo.thumbnailAlt} />
                 </div>
                 <div className="article-title-container">
                     {variant === 'large' && (
-                        <span className="tag">{articleInfo.tag}</span>
+                        <span className="category">{articleInfo.category}</span>
                     )}
                     <h2 className="title">{articleInfo.title}</h2>
-                    <p className="excerpt">{articleInfo.getExcerpt()}</p>
+                    {variant === 'medium' && (
+                        <p className="excerpt">{articleInfo.getExcerpt()}</p>
+                    )}
                     <div className="author">
-                        <InitialsIcon initials={articleInfo.author.getInitials()} />
+                        {variant === 'medium' && (
+                            <InitialsIcon initials={articleInfo.author.getInitials()} />
+                        )}
                         <span className="name">{articleInfo.author.name}</span>
                         <span className="department">{articleInfo.author.department}</span>
                     </div>
