dc7e64967962ddd88c27a9191264ef3ecff05dcc
hwas2620 <hwas2620@gmail.com>
2025-08-28 15:50:52 +0900

    day18

---
+++ study/day18/main.py
@@ -0,0 +1,20 @@
+##### 파이썬 데이터과학
+### numpy
+# 다음 NumPy 코드의 실행 결과는?
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+result = arr1 * 2 + arr2 // 2
+
+print(result) # [4 7 9 12]
+
+# 다음 NumPy 코드의 실행 결과는?
+arr = np.array([[1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12]])
+
+result = arr[1:, 1::2]
+print(result) # [[ 6  8] [10 12]]
+
+### jupyter lab
